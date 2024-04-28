577e3379e3a1cc981def90d9b8ae593736d66aca
ivanbensa89 <150074082+ivanbensa89@users.noreply.github.com>
2024-04-28 07:59:02 +0200

    Update script.js

--- 10_functions/script.js
+++ 10_functions/script.js
@@ -1,6 +1,11 @@
-document.getElementById("Btn").addEventListener("click", function() {
-    var cena = 150;
+function izracunajSaPDV(cena) {
     var pdv = cena * 0.18;
     var ukupnaCena = cena + pdv;
+    return ukupnaCena;
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+    var cena = 150;
+    var ukupnaCena = izracunajSaPDV(cena);
     alert("Cena sa PDV-om (18%): " + ukupnaCena + "€");
 });
